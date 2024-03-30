af429faf53de0ae641ec35c2ec6e189381bfe7c2
ThankGod Okoro <ebuzzadvert@gmail.com>
2024-03-30 14:32:23 +0100

    podcast coming soon added

--- app/podcast.php
+++ app/podcast.php
@@ -8,17 +8,18 @@
             <?php include "./components/topnav.php"; ?>
             <div class="flex-fill overflow-y-lg-auto scrollbar bg-body rounded-top-4 rounded-top-start-lg-4 rounded-top-end-lg-0 border-top border-lg shadow-2">
                 <main class="container-fluid px-3 py-5 p-lg-6 p-xxl-8">
-                    <div class="mb-6 mb-xl-10">
+                    <!-- <div class="mb-6 mb-xl-10">
                         <div class="row g-3 align-items-center">
                             <div class="col">
                                 <h2 class="ls-tight">Podcast</h2>
                             </div>
                         </div>
-                    </div>
+                    </div> -->
 
                     <div class="container">
-                        <div class="card">
-                            <div class="card-body pb-0">
+                        <div class="card border-0">
+                            <div class="card-body pb-0 text-center">
+                                <img src="./assets/img/coming-soon.jpg" alt="coming soon" width="600">
                             </div>
                         </div>
                     </div>
