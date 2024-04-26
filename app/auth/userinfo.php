ee5c6b9020361575d78c673675fa4e4b31002edd
Dami Akindele <damiakin95@gmail.com>
2024-04-26 11:09:33 +0100

    fixed id bug | updated code

--- app/auth/userinfo.php
+++ app/auth/userinfo.php
@@ -3,12 +3,12 @@
 include "./config/db.php";
 
 
-$select_query = "SELECT * FROM users WHERE user_id ='".$_SESSION['user_id']."'";
+$select_query = "SELECT * FROM users WHERE user_id ='" . $_SESSION['user_id'] . "'";
 $result = mysqli_query($conn, $select_query);
 if (mysqli_num_rows($result) > 0) {
     // output data of each row
-    while($row = mysqli_fetch_assoc($result)) {
-        $id = $row['id'];
+    while ($row = mysqli_fetch_assoc($result)) {
+        $id = $row['user_id'];
         $first_name = $row['first_name'];
         $last_name = $row['last_name'];
         $picture = $row['picture'];
@@ -17,11 +17,11 @@ if (mysqli_num_rows($result) > 0) {
 }
 
 
-$select_diet_query = "SELECT * FROM userdiet WHERE userID ='".$_SESSION['user_id']."'";
+$select_diet_query = "SELECT * FROM userdiet WHERE userID ='" . $_SESSION['user_id'] . "'";
 $result = mysqli_query($conn, $select_diet_query);
 if (mysqli_num_rows($result) > 0) {
     // output data of each row
-    while($row = mysqli_fetch_assoc($result)) {
+    while ($row = mysqli_fetch_assoc($result)) {
         $diet_id = $row['diet_id'];
         $userID = $row['userID'];
         $diet = $row['diet'];
