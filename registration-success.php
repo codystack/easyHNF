e0d1db889e02033e087707b96a38714e6db3d8bb
codystack <ebuzzadvert@gmail.com>
2024-05-01 22:07:49 +0100

    spam box message added

--- registration-success.php
+++ registration-success.php
@@ -99,6 +99,7 @@
                                             <h6 class="h3 mb-1">Welcome to the Easy Life👏</h6>
                                             <!-- <p class="h6 mb-0">Login and start your to a healthier life.</p> -->
                                             <p class="h6 mb-0">An email with a verification link has been sent to your email (<?php echo $_SESSION['email']; ?>)</p>
+                                            <p class="h6 mb-0">Check spam box if you're yet to receive the email.</p>
                                             <hr class="mb-2 mt-2">
                                             <p class="h6 mb-2">Please verify your email<br>
                                                 to gain access to your account.</p>
