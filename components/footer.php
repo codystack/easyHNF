fe87f1afeb1d0f9098b437b25a498e5c1e9d8b9c
Dami Akindele <damiakin95@gmail.com>
2024-06-11 19:46:25 +0100

    added login links to plans and updated blog post

--- components/footer.php
+++ components/footer.php
@@ -1,136 +1,138 @@
-    
-        
-        <div class="section footer-section">
+    <div class="section footer-section">
 
-            <div class="footer-widget-section section-padding">
+        <div class="footer-widget-section section-padding">
 
-                <div class="container">
+            <div class="container">
 
-                    <div class="footer-widget-wrapper">
-                        <div class="row">
-                            <div class="col-lg-4">
+                <div class="footer-widget-wrapper">
+                    <div class="row">
+                        <div class="col-lg-4">
+
+                            <div class="footer-widget">
+                                <h3 class="footer-widget-title">Subscribe to our Newsletter</h3>
+
+                                <div class="widget-newsletter">
+                                    <p>Get a free copy of our bi-annual newsletter on health, nutrition and fitness.</p>
+
+                                    <div class="newsletter-form">
+                                        <form action="#">
+                                            <input type="text" placeholder="Enter email">
+                                            <button class="btn btn-primary btn-hover-white">Subscribe Now</button>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-8">
+
+                            <div class="widget-wrapper">
 
                                 <div class="footer-widget">
-                                    <h3 class="footer-widget-title">Subscribe to our Newsletter</h3>
+                                    <h3 class="footer-widget-title">All Plans</h3>
 
-                                    <div class="widget-newsletter">
-                                        <p>Get a free copy of our bi-annual newsletter on health, nutrition and fitness.</p>
+                                    <ul class="widget-link">
+                                        <li><a href="easyhnf.com/login">Basic</a>
+                                            <ul class="dropdown" style="margin-left: 20px;">
+                                                <li><a href="easyhnf.com/login">- 1,200 calories plan</a></li>
+                                                <li><a href="easyhnf.com/login">- 1,500 calories</a></li>
+                                                <li><a href="easyhnf.com/login">- 1,500 calories high protein</a></li>
+                                                <li><a href="easyhnf.com/login">- 1,500 blood sugar friendly plan</a></li>
+                                                <li><a href="easyhnf.com/login">- 1,500 DASH</a></li>
+                                                <li><a href="easyhnf.com/login">- Cholesterol friendly plan</a></li>
+                                            </ul>
+                                        </li>
 
-                                        <div class="newsletter-form">
-                                            <form action="#">
-                                                <input type="text" placeholder="Enter email">
-                                                <button class="btn btn-primary btn-hover-white">Subscribe Now</button>
-                                            </form>
-                                        </div>
-                                    </div>
+                                        <li><a href="easyhnf.com/login">Custom </a></li>
+                                        <li><a href="easyhnf.com/login">Nutrition coaching</a></li>
+                                    </ul>
                                 </div>
-                            </div>
-                            <div class="col-lg-8">
 
-                                <div class="widget-wrapper">
+                                <div class="footer-widget">
+                                    <h3 class="footer-widget-title">Other useful Links</h3>
 
-                                    <div class="footer-widget">
-                                        <h3 class="footer-widget-title">All Plans</h3>
-
-                                        <ul class="widget-link">
-                                            <li><a href="#">Basic</a>
-                                                <ul class="dropdown" style="margin-left: 20px;">
-                                                    <li><a href="#">- 1,200 calories plan</a></li>
-                                                    <li><a href="#">- 1,500 calories</a></li>
-                                                    <li><a href="#">- 1,500 calories high protein</a></li>
-                                                    <li><a href="#">- 1,500 blood sugar friendly plan</a></li>
-                                                    <li><a href="#">- 1,500 DASH</a></li>
-                                                    <li><a href="#">- Cholesterol friendly plan</a></li>
-                                                </ul>
-                                            </li>
-                                            
-                                            <li><a href="#">Custom </a></li>
-                                            <li><a href="#">Nutrition coaching</a></li>
-                                        </ul>
-                                    </div>
-
-                                    <div class="footer-widget">
-                                        <h3 class="footer-widget-title">Other useful Links</h3>
-
-                                        <ul class="widget-link">
-                                            <li><a href="privacy">Privacy Policy</a></li>
-                                            <li><a href="terms">Terms & Conditions</a></li>
-                                            <li><a href="javascript:void(Tawk_API.toggle())">Customer Support</a></li>
-                                            <li><a href="faq">FAQ’s</a></li>
-                                        </ul>
-                                    </div>
-
-                                    <div class="footer-widget">
-                                        <h3 class="footer-widget-title">Social Media</h3>
-
-                                        <ul class="widget-link">
-                                            <li><a href="https://web.facebook.com/easyHNF" target="_blank">Facebook</a></li>
-                                            <li><a href="https://twitter.com/easyhnf?s=11&t=kR7TeOfAySMs5o5rlTNnRQ" target="_blank">X (formerly twitter)</a></li>
-                                            <li><a href="https://www.instagram.com/easyhnf?igsh=MzRlODBiNWFlZA==" target="_blank">Instagram</a></li>
-                                            <li><a href="https://www.linkedin.com/company/easyhnf/" target="_blank">Linkedin</a></li>
-                                        </ul>
-                                    </div>
-
+                                    <ul class="widget-link">
+                                        <li><a href="privacy">Privacy Policy</a></li>
+                                        <li><a href="terms">Terms & Conditions</a></li>
+                                        <li><a href="javascript:void(Tawk_API.toggle())">Customer Support</a></li>
+                                        <li><a href="faq">FAQ’s</a></li>
+                                    </ul>
                                 </div>
+
+                                <div class="footer-widget">
+                                    <h3 class="footer-widget-title">Social Media</h3>
+
+                                    <ul class="widget-link">
+                                        <li><a href="https://web.facebook.com/easyHNF" target="_blank">Facebook</a></li>
+                                        <li><a href="https://twitter.com/easyhnf?s=11&t=kR7TeOfAySMs5o5rlTNnRQ" target="_blank">X (formerly twitter)</a></li>
+                                        <li><a href="https://www.instagram.com/easyhnf?igsh=MzRlODBiNWFlZA==" target="_blank">Instagram</a></li>
+                                        <li><a href="https://www.linkedin.com/company/easyhnf/" target="_blank">Linkedin</a></li>
+                                    </ul>
+                                </div>
+
                             </div>
                         </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+        <div class="footer-copyright">
+            <div class="container">
+
+                <div class="copyright-wrapper">
+
+                    <div class="copyright-text">
+                        <p>&copy; <script>
+                                document.write(new Date().getFullYear());
+                            </script> <span> EasyHNF&reg; </span> All rights reserved.</p>
+                    </div>
+
+                    <div class="copyright-link">
+                        <a href="terms">Terms of Service</a>
+                        <a href="privacy">Privacy Policy</a>
                     </div>
 
                 </div>
 
             </div>
-
-            <div class="footer-copyright">
-                <div class="container">
-
-                    <div class="copyright-wrapper">
-
-                        <div class="copyright-text">
-                            <p>&copy; <script>document.write(new Date().getFullYear());</script> <span> EasyHNF&reg; </span> All rights reserved.</p>
-                        </div>
-
-                        <div class="copyright-link">
-                            <a href="terms">Terms of Service</a>
-                            <a href="privacy">Privacy Policy</a>
-                        </div>
-
-                    </div>
-
-                </div>
-            </div>
-
         </div>
 
-        </div>
+    </div>
 
-        <script src="assets/js/vendor/modernizr-3.11.2.min.js"></script>
-        <script src="assets/js/vendor/jquery-3.5.1.min.js"></script>
+    </div>
 
-        <script src="assets/js/plugins/popper.min.js"></script>
-        <script src="assets/js/plugins/bootstrap.min.js"></script>
+    <script src="assets/js/vendor/modernizr-3.11.2.min.js"></script>
+    <script src="assets/js/vendor/jquery-3.5.1.min.js"></script>
 
-        <script src="assets/js/plugins/swiper-bundle.min.js"></script>
-        <script src="assets/js/plugins/jquery.magnific-popup.min.js"></script>
-        <script src="assets/js/plugins/jquery.nice-select.min.js"></script>
-        <script src="assets/js/plugins/video-playlist.js"></script>
-        <script src="assets/js/plugins/ajax-contact.js"></script>
+    <script src="assets/js/plugins/popper.min.js"></script>
+    <script src="assets/js/plugins/bootstrap.min.js"></script>
 
-        <script src="assets/js/main.js"></script>
+    <script src="assets/js/plugins/swiper-bundle.min.js"></script>
+    <script src="assets/js/plugins/jquery.magnific-popup.min.js"></script>
+    <script src="assets/js/plugins/jquery.nice-select.min.js"></script>
+    <script src="assets/js/plugins/video-playlist.js"></script>
+    <script src="assets/js/plugins/ajax-contact.js"></script>
 
-        <!--Start of Tawk.to Script-->
-        <script type="text/javascript">
-            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-            (function(){
-            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-            s1.async=true;
-            s1.src='https://embed.tawk.to/660807b3a0c6737bd1267a8e/1hq7ls5s0';
-            s1.charset='UTF-8';
-            s1.setAttribute('crossorigin','*');
-            s0.parentNode.insertBefore(s1,s0);
-            })();
-        </script>
-        <!--End of Tawk.to Script-->
+    <script src="assets/js/main.js"></script>
 
-        </body>
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+        var Tawk_API = Tawk_API || {},
+            Tawk_LoadStart = new Date();
+        (function() {
+            var s1 = document.createElement("script"),
+                s0 = document.getElementsByTagName("script")[0];
+            s1.async = true;
+            s1.src = 'https://embed.tawk.to/660807b3a0c6737bd1267a8e/1hq7ls5s0';
+            s1.charset = 'UTF-8';
+            s1.setAttribute('crossorigin', '*');
+            s0.parentNode.insertBefore(s1, s0);
+        })();
+    </script>
+    <!--End of Tawk.to Script-->
 
-        </html>
+    </body>
+
+    </html>
