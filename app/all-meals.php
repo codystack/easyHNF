58dafbe1d1806fd2518d1937e6e488c111653dfe
codystack <ebuzzadvert@gmail.com>
2024-06-11 17:43:03 +0100

    Text Error

--- app/all-meals.php
+++ app/all-meals.php
@@ -73,7 +73,7 @@
                                             $_SESSION['sub_error_message'] = "Buy a subscription plan to view meals";
                                         }
 
-                                    $select_query = "SELECT * FROM meals CROSS JOIN basic_meal_plan WHERE basic_meal_plan.plan_id = '$planID'";
+                                    $select_query = "SELECT * FROM meals INNER JOIN basic_meal_plan WHERE basic_meal_plan.plan_id = '$planID'";
                                         $result = mysqli_query($conn, $select_query);
                                         if (mysqli_num_rows($result) > 0) {
                                             // output data of each row
@@ -83,6 +83,7 @@
                                                 $title = $row['title'];
                                                 $meal_image = $row['meal_image'];
                                     ?>
+                                    <?php echo $planID; ?>
                                     <div class="col-xl-3 col-sm-6">
                                         <div class="card">
                                             <a href="view-meal?id=<?php echo $meal_id; ?>">
